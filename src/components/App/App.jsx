d3b3f079c4a9255bb0ff014ba0a6a106376d7427
Roman Lysenko <rom.lysenko.23@gmail.com>
2022-02-27 22:46:57 +0200

    Adds initial files

---
+++ src/components/App/App.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+
+class App extends Component {
+
+
+  render() {
+    return (
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          textTransform: 'uppercase',
+          color: '#010101',
+        }}
+      >
+        React homework template
+      </div>
+    );
+  };
+};
+
+export default App;
