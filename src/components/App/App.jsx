ca053de393028a58ae965cf0a8d9285aa372ac2c
Roman Lysenko <rom.lysenko.23@gmail.com>
2022-03-01 02:52:48 +0200

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,10 @@
 import React, { Component } from "react";
+import Searchbar from 'components/Searchbar';
+import ImageGallery from 'components/ImageGallery';
+import ImageGalleryItem from 'components/ImageGalleryItem';
 import Modal from 'components/Modal';
+import Button from 'components/Button';
+import Loader from 'components/Loader';
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 import { AppWrapper } from "./App.styled";
 
